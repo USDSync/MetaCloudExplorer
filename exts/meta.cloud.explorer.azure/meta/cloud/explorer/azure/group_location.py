f86a37453454651e94b15f53e473345191186fa7
cryptobabies <endiverse@gmail.com>
2022-08-27 21:06:33 -0700

    massive checkpoint, amazing!!

--- exts/meta.cloud.explorer.azure/meta/cloud/explorer/azure/group_location.py
+++ exts/meta.cloud.explorer.azure/meta/cloud/explorer/azure/group_location.py
@@ -4,6 +4,17 @@ from pxr import Gf, UsdGeom, UsdLux, Usd, Sdf
 from .math_utils import calcPlaneSizeForGroup
 from .prim_utils import cleanup_prim_path
 import locale 
+import asyncio
+import carb
+
+import omni.client
+import omni.kit.app
+import omni.ui as ui
+import omni.usd
+import omni.kit.commands
+
+
+
 
 class LocGrpView(GroupBase):
     def __init__(self, viewPath:str, scale:float, upAxis:str, shapeUpAxis:str, symPlanes:bool):
@@ -17,6 +28,10 @@ class LocGrpView(GroupBase):
         super().__init__()
 
     def calcGroupPlaneSizes(self):
+
+        self._dataStore._lcl_groups = []
+        self._dataStore._lcl_sizes = []
+
         if len(self._dataStore._location_count) == 0:
             self._dataManager.refresh_data()
 
@@ -24,7 +39,7 @@ class LocGrpView(GroupBase):
         if len(self._dataStore._location_count) == 0:
             return 0
 
-        self.view_path = Sdf.Path(self.root_path.AppendPath('Loc'))
+        self.view_path = Sdf.Path(self.root_path.AppendPath(self._view_path))
 
         gpz = self._dataStore._location_count.copy()
         for grp in gpz:
@@ -53,13 +68,13 @@ class LocGrpView(GroupBase):
     #Abstact to load resources
     def loadResources(self):      
 
-        self.view_path = Sdf.Path(self.root_path.AppendPath('Loc'))
+        self.view_path = Sdf.Path(self.root_path.AppendPath(self._view_path))
 
         if (len(self._dataStore._lcl_groups)) >0 :
 
             #Cycle all the loaded groups
             for grp in self._dataStore._lcl_groups:
-                print(grp)
+                carb.log_info(grp)
 
                 #Cleanup the group name for a prim path
                 group_prim_path = self.view_path.AppendPath(grp["group"])
@@ -69,15 +84,13 @@ class LocGrpView(GroupBase):
 
                     #Is this the group?
                     if key == grp["group"]:
-
-                        self.loadGroupResources(group_prim_path, values)
-
+                        self.loadGroupResources(key, group_prim_path, values)
 
     def selectGroupPrims(self):
         
         self.paths = []
 
-        base = Sdf.Path("/Subs")
+        base = Sdf.Path(self.root_path.AppendPath(self._view_path))
 
         for grp in self._dataStore._map_location.keys():
             grp_path = base.AppendPath(cleanup_prim_path(self, grp))
--- exts/meta.cloud.explorer.azure/meta/cloud/explorer/azure/group_sub.py
+++ exts/meta.cloud.explorer.azure/meta/cloud/explorer/azure/group_sub.py
@@ -4,7 +4,8 @@ from pxr import Gf, UsdGeom, UsdLux, Usd, Sdf
 from .math_utils import calcPlaneSizeForGroup
 from .prim_utils import cleanup_prim_path, create_and_place_prim, get_parent_child_prim_path
 import locale 
-
+import asyncio
+import carb
 import omni.client
 import omni.kit.app
 import omni.ui as ui
@@ -15,7 +16,7 @@ import omni.kit.commands
 #--- SUBSCRIPTION BASED GROUPS
 class SubGrpView(GroupBase):
     def __init__(self, viewPath:str, scale:float, upAxis:str, shapeUpAxis:str, symPlanes:bool):
-        self._root_path = Sdf.Path(viewPath)
+
         self._scale = scale
         self._upAxis = upAxis
         self._shapeUpAxis = shapeUpAxis
@@ -25,6 +26,10 @@ class SubGrpView(GroupBase):
         super().__init__()
 
     def calcGroupPlaneSizes(self):
+
+        self._dataStore._lcl_groups = []
+        self._dataStore._lcl_sizes = []
+
         if len(self._dataStore._subscription_count) == 0:
             self._dataManager.refresh_data()
 
@@ -32,7 +37,7 @@ class SubGrpView(GroupBase):
         if len(self._dataStore._subscription_count) == 0:
             return 0
         
-        self.view_path = Sdf.Path(self.root_path.AppendPath('Subs'))
+        self.view_path = Sdf.Path(self.root_path.AppendPath(self._view_path))
 
         #temp group list to prep for planes, adds to main aggregate
         gpz = self._dataStore._subscription_count.copy()
@@ -61,13 +66,13 @@ class SubGrpView(GroupBase):
    #Abstact to load resources
     def loadResources(self):      
 
-        self.view_path = Sdf.Path(self.root_path.AppendPath('Subs'))
+        self.view_path = Sdf.Path(self.root_path.AppendPath(self._view_path))
 
         if (len(self._dataStore._lcl_groups)) >0 :
 
             #Cycle all the loaded groups
             for grp in self._dataStore._lcl_groups:
-                print(grp)
+                carb.log_info(grp)
 
                 #Cleanup the group name for a prim path
                 group_prim_path = self.view_path.AppendPath(grp["group"])
@@ -77,9 +82,7 @@ class SubGrpView(GroupBase):
 
                     #Is this the group?
                     if key == grp["group"]:
-
-                        self.loadGroupResources(group_prim_path, values)
-
+                        self.loadGroupResources(key, group_prim_path, values)
     
     def selectGroupPrims(self):
         
